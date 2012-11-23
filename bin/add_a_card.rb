3e28b9a82c0ba114b60d125d978ece8e5c69a45a
Errin Larsen <elarsen@insightcards.com>
2012-11-22 18:07:57 -0600

    Add TrelloAPI#create_card and refactor test script

--- bin/add_a_card.rb
+++ bin/add_a_card.rb
@@ -4,21 +4,16 @@ require "bundler/setup"
 $LOAD_PATH.unshift File.dirname(__FILE__) + "/../lib"
 require "recurello"
 
-key_file_name = File.join(File.dirname(__FILE__), "..", "trello_keys.yml")
-keys = Recurello::TrelloAPI::Keys.new(File.open(key_file_name, "r"))
-
+keys = Recurello::TrelloAPI::Keys.new(File.open("trello_keys.yml", "r"))
 trello = Recurello::TrelloAPI.new(keys)
 
-me = trello.member.find("errinlarsen")
-boards = me.boards
-todo_board = boards.find { |b| b.name == "Todo" }
-lists = todo_board.lists
-next_list = lists.find { |l| l.name == "Next" }
-
 options = {
-  name:         "Test card #1",
-  list_id:      next_list.id,
-  description:  "This is it!"
+  member: "errinlarsen",
+  board:  "Todo",
+  list:   "Next",
+  name:   "Test card #1",
+  desc:   "This is it!"
 }
 
-trello.create_card(options)
+new_card = trello.create_card(options)
+puts new_card.inspect
--- lib/recurello/trello_api.rb
+++ lib/recurello/trello_api.rb
@@ -22,6 +22,30 @@ module Recurello
       authorize(keys)
     end
 
+    def create_card(opts)
+      member_name = opts.fetch(:member)
+      board_name  = opts.fetch(:board)
+      list_name   = opts.fetch(:list)
+      card_name   = opts.fetch(:name)
+      desc_str    = opts.fetch(:desc)
+
+      member = Member.find(member_name)
+
+      boards = member.boards
+      board = boards.find { |b| b.name == board_name }
+
+      lists = board.lists
+      list = lists.find { |l| l.name == list_name }
+
+      card_creation_options = {
+        list_id:      list.id,
+        name:         card_name,
+        description:  desc_str
+      }
+
+      Card.create(card_creation_options)
+    end
+
     private
     def authorize(keys)
       Trello::Authorization.const_set :AuthPolicy, OAuthPolicy
