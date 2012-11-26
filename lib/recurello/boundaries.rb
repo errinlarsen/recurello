ca9c50d7cb8ea181c326750d6bac709838b62bcc
Errin Larsen <elarsen@insightcards.com>
2012-11-25 19:03:50 -0600

    Clean up the Boundaries moduels

--- lib/recurello/boundaries.rb
+++ lib/recurello/boundaries.rb
@@ -1,13 +1,11 @@
 module Recurello
   module ServerBoundary
     def response
-      return {name: card.name, message: card.message}
     end
   end
-  
+
   module ClientBoundary
-    def self.included(base)
-      base.send(:attr_reader, :request)
+    def request
     end
   end
 end
