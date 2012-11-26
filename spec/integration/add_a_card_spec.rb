b04677eda9e28bb94c9f07df3ab31591eaad9ba6
Errin Larsen <elarsen@insightcards.com>
2012-11-26 08:17:33 -0600

    Fix integration test with proper mocks

--- spec/integration/add_a_card_spec.rb
+++ spec/integration/add_a_card_spec.rb
@@ -1,23 +1,39 @@
 require "recurello"
+require "trello_api"
 
 describe "Adding a Card to a Trello Board's List", :needs => :trello_api do
   it "creates a Card on the Card's Board in the Card's List" do
     card = Recurello::Card.new(
-      member_name:  "errinlarsen",
-      board_name:   "Todo",
-      list_name:    "Next",
-      name:         "Test card #1",
-      desc:         "This is it!"
+      member_name:  "some_member",
+      board_name:   "Some Board",
+      list_name:    "Some List",
+      name:         "Some Card Name",
+      desc:         "Some card description"
     )
 
     trello_api = TrelloAPI::Wrapper.new(fake_key_file)
-    trello_api.authorize
+    trello_api.setup_credentials
+
+    trello_api.stub(:find_list_id) do |member_name, board_name, list_name|
+      member_name.should == "some_member"
+      board_name.should == "Some Board"
+      list_name.should == "Some List"
+      3 # Fake listID returned
+    end
+
+    trello_api.stub(:create_card) do |opts|
+      opts.should include(
+        :list_id => 3,
+        :name => "Some Card Name",
+        :description => "Some card description"
+      )
+    end
 
     list_finder = Recurello::ListFinder.new(trello_api)
     card.list_id = list_finder.find_list_id_from_card(card)
 
     card_creator = Recurello::CardCreator.new(trello_api)
-    new_card = card_creator.create(card)
+    card_creator.create(card)
   end
 
   def fake_key_file
